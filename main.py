eb50854bd144383df6cb384bd36853668846c477
botrott <padgetalex@yandex.ru>
2024-01-23 18:50:57 +0300

    add traffic_analys

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtGui import QFontDatabase
 
 from ui.topology import show_topology
-from ui.traffic_analysis import show_traffic_analysis
+from ui.traffic_analysis import TrafficAnalyzer
 from ui.interface_data import show_interface_data
 from ui.device_availability import DeviceAvailability
 from ui.port_scanner import PortScanner
@@ -25,27 +25,27 @@ class MyApp(QMainWindow):
 
         btn_traffic_analysis = QPushButton('Анализ сетевого трафика', self)
         btn_traffic_analysis.setGeometry(50, 100, 400, 50)
-        btn_traffic_analysis.clicked.connect(show_traffic_analysis)
+        btn_traffic_analysis.clicked.connect(self.show_traffic_analysis)
         btn_traffic_analysis.setStyleSheet("text-align: center;")
 
-        btn_interface_data = QPushButton('Опрос и обработка данных\n о сетевых интерфейсах', self)
-        btn_interface_data.setGeometry(50, 150, 400, 80)
-        btn_interface_data.clicked.connect(show_interface_data)
-        btn_interface_data.setStyleSheet("text-align: center;")
-
         btn_device_availability = QPushButton('Проверка доступности сетевых\n устройств', self)
-        btn_device_availability.setGeometry(50, 210, 400, 80)
+        btn_device_availability.setGeometry(50, 150, 400, 80)
         btn_device_availability.clicked.connect(self.show_device_availability)
         btn_device_availability.setStyleSheet("text-align: center;")
 
         btn_port_scanner = QPushButton('Порт-сканер', self)
-        btn_port_scanner.setGeometry(50, 280, 400, 50)
+        btn_port_scanner.setGeometry(50, 230, 400, 50)
         btn_port_scanner.clicked.connect(self.show_port_scanner)
         btn_port_scanner.setStyleSheet("text-align: center;")
 
-        self.setGeometry(300, 300, 480, 400)
+        self.setGeometry(700, 300, 480, 370)
         # self.setFixedSize(480, 400)
         self.show()
+
+    def show_traffic_analysis(self):
+        self.hide()
+        self.device_availability_window = TrafficAnalyzer()
+        self.device_availability_window.show()
 
     def show_device_availability(self):
         self.hide()
@@ -65,4 +65,7 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     QFontDatabase.addApplicationFont("fonts/Montserrat-Regular.ttf")
     ex = MyApp()
-    sys.exit(app.exec_())
+    try:
+        sys.exit(app.exec_())
+    except Exception as e:
+        print("Unhandled exception:", e)
--- ui/traffic_analysis.py
+++ ui/traffic_analysis.py
@@ -1,2 +1,59 @@
-def show_traffic_analysis(self):
-    print('Анализ сетевого трафика')
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QTextBrowser, QPushButton
+from PyQt5.QtCore import QThread, pyqtSignal
+from scapy.all import sniff
+
+
+class TrafficThread(QThread):
+    packet_received = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+        self.is_running = False
+
+    def run(self):
+        self.is_running = True
+
+        def packet_handler(packet):
+            if self.is_running:
+                self.packet_received.emit(packet.summary())
+
+        sniff(iface='Ethernet', prn=packet_handler)
+
+    def stop(self):
+        self.is_running = False
+
+
+class TrafficAnalyzer(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet(open('styles/style_port_scanner.css').read())
+        self.setWindowTitle("Анализ трафика")
+        self.layout = QVBoxLayout()
+
+        self.result_label = QLabel("Результат:")
+        self.layout.addWidget(self.result_label)
+
+        self.result_browser = QTextBrowser()
+        self.layout.addWidget(self.result_browser)
+
+        self.start_button = QPushButton("Старт")
+        self.start_button.clicked.connect(self.start_traffic_analysis)
+        self.layout.addWidget(self.start_button)
+
+        self.stop_button = QPushButton("Стоп")
+        self.stop_button.clicked.connect(self.stop_traffic_analysis)
+        self.layout.addWidget(self.stop_button)
+
+        self.setLayout(self.layout)
+
+    def start_traffic_analysis(self):
+        self.result_browser.clear()
+        self.traffic_thread = TrafficThread()
+        self.traffic_thread.packet_received.connect(self.update_results)
+        self.traffic_thread.start()
+
+    def stop_traffic_analysis(self):
+        self.traffic_thread.stop()
+
+    def update_results(self, packet_summary):
+        self.result_browser.append(packet_summary)
